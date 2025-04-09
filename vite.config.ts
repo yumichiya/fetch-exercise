6c3d22875f07b2b5350a2d83096b916f3974ebe0
Jasmine Nguyen <yukariaimi@gmail.com>
2025-04-08 22:42:08 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fetch-exercise"
+  base: "/fetch-exercise",
+  server: {
+    cors: {
+      // the origin you will be accessing via browser
+      origin: 'https://yumichiya.github.io/fetch-exercise/',
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
 })
